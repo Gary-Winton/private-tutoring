9e7a0d1188a6f534299ff502802cfdd46aac1b29
Gary <gary-winton@live.nmit.ac.nz>
2020-06-18 11:55:30 +1200

    functions

---
+++ index.js
@@ -0,0 +1,43 @@
+// function area(b, h) {
+// return 0.5 * b * h
+// }
+// console.log(area(4,3));
+// // calling function and perameters
+
+
+// function rectangle(w, h) {
+//     return w * h
+// }
+// console.log(rectangle(10, 5))
+// // * the complete box size
+
+// function circle(r) {
+//     return 2*3.14*r
+// }
+// console.log(circle(1))
+// // because the perameters is only 1 function, you only need 1 number in the console.log
+
+// function square(a) {
+//     return Math.pow(a,2)
+// }
+// console.log(square(3))
+// // a with the power of 2 a*a
+
+// function name(firstname) {
+//     //parameter is the space holder
+//     return "Hello " +  firstname
+// }
+// console.log(name("Gary"));
+
+// function fullName(x, y, z) {
+//     return "Hello " + x +" "+ y +" "+ z
+// }
+// console.log(fullName("Gary", "Stuart ", "Winton"))
+
+function num(x ,y , z) {
+    return (x * y) + (0.5 * z)
+    // 
+}
+console.log(num(1, 3, 5 ))
+
+    
